f907d3da025af75aa83dd44b57bb6634441f9a78
Dhaval Purohit <dhaval.purohit08@gmail.com>
2016-09-15 12:39:53 +0530

    adds events form

---
+++ app/routes/events/new.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	model() {
+		return this.store.createRecord('event');
+	},
+
+	actions: {
+		saveEvent(newEvent) {
+			newEvent.save().then(() => this.transitionTo('index'));
+		},
+
+		willTransition() {
+			// rollbackAttributes() removes the record from the store
+      // if the model 'isNew'
+      this.controller.get('model').rollbackAttributes();
+		}
+	}
+});
